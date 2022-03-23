ddce6851fda23bbf5c43ef05b457f6ab6dbed1ff
ryanromaris <ryanromaris@naver.com>
2022-03-23 20:33:14 +0900

    Landing(IconGroup): ts 적용 및 리팩토링

---
+++ components/IconGroup.tsx
@@ -0,0 +1,41 @@
+import Image from 'next/dist/client/image';
+import eastern_icon from '../constant/icon/eastern.webp';
+import extra_icon from '../constant/icon/extra.webp';
+import fantasy_icon from '../constant/icon/fantasy.webp';
+import modern_icon from '../constant/icon/modern.webp';
+import noir_icon from '../constant/icon/noir.webp';
+import sf_icon from '../constant/icon/sf.webp';
+
+interface IIconGroupProps {
+  iconSrc: StaticImageData;
+  iconName: String;
+}
+
+function IconGroup() {
+  const Icons: IIconGroupProps[] = [
+    { iconSrc: fantasy_icon, iconName: '판타지/중세' },
+    { iconSrc: eastern_icon, iconName: '동양/사극/무협' },
+    { iconSrc: sf_icon, iconName: 'SF/아포칼립스' },
+    { iconSrc: noir_icon, iconName: '느와르/범죄/재벌' },
+    { iconSrc: modern_icon, iconName: '일상/현대물' },
+    { iconSrc: extra_icon, iconName: '엑스트라/소재' },
+  ];
+
+  //서버에서 받아온 데이터 __ 임의로 추가해줌.
+  return (
+    <>
+      <div className="icon_group_container">
+        {Icons.map((icon) => (
+          <div className="icon_box">
+            <div className="icon_image">
+              <Image src={icon.iconSrc} />
+            </div>
+            <div>{icon.iconName}</div>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+}
+
+export default IconGroup;
